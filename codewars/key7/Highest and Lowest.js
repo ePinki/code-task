41f342cded7103040e0d898b0b0e70a9ae59bc9f
Nazar Yarmolchyk <yrnazarusa@gmail.com>
2023-01-12 20:13:18 +0300

    +задача

---
+++ codewars/key7/Highest and Lowest.js
@@ -0,0 +1,10 @@
+function highAndLow(numbers){
+  numbers = numbers.split(' ').map(Number);
+  return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+}
+
+
+function highAndLow(numbers){
+  let arr = numbers.split(' ').map(Number);  
+  return Math.max(...arr) + ' ' + Math.min(...arr);
+}
